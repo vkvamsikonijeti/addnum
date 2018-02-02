32bef361409fedbce81e056c943e86e8cb766848
vkvamsikonijeti <vkvamsikonijeti@gmail.com>
2018-02-02 18:28:56 +0530

    Modified Sum.groovy

--- src/org/addition/Sum.groovy
+++ src/org/addition/Sum.groovy
@@ -1,18 +1,18 @@
 package org.addition
 
-class Sum{
-   def num1
-   def num2
- 
-  Sum (num1,num2){
-   this.num1=num1
-   this.num2=num2
-}
+class Sum {
+   def config
+   def script
 
+ 
+  Sum (script,config) {
+   this.config= config
+   this.script= script
+ }
+}
 addition(){
 this.script.stage('adding'){
-this.script.echo this.num1 + this.num2 
-}
-}
+this.script.echo this.config.num1 + this.config.num2 
+ }
 }
 
